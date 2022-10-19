e010269c48c43d7b8bdb26f8971bf768f1161fcb
matyas <matthias.sajtos@concircle.com>
2022-10-19 20:48:35 +0200

    🔨 Add timer

---
+++ index.js
@@ -0,0 +1,13 @@
+import clipboard from 'clipboardy';
+
+let clearedText = '';
+setInterval(function () {
+    let clippedText = clipboard.readSync();
+    if (clippedText !== clearedText) {
+        console.log(clippedText);
+        clearedText = '' + clippedText;
+        clipboard.writeSync(clearedText);
+    }
+},1000)
+
+
